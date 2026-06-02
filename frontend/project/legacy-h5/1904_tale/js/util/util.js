84d51fe084fec7ce3ff5323c2b93db16d48be463
zhangrh <runhaozhang@sohu-inc.com>
2026-06-02 12:26:33 +0800

    refactor: 清理 legacy-h5 旧服务依赖残留

--- frontend/project/legacy-h5/1904_tale/js/util/util.js
+++ frontend/project/legacy-h5/1904_tale/js/util/util.js
@@ -4,18 +4,5 @@ export default {
     var Rand = Math.random()
     var num = Min + Math.round(Rand * Range) // 四舍五入
     return num
-  },
-  bindWX: function () {
-  },
-  getQueryString: function (name) { // 截取链接上的指定参数
-    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i')
-    var r = window.location.search.substr(1).match(reg)
-    if (r != null) return unescape(r[2])
-    return null
-  },
-  eventTracking: function () {
-  },
-  getUserh5Cid: function () {
-    return ''
   }
 }
